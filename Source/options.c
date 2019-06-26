f40936d38dce84b1576139f784b648f9a707e964
Nikolai Zhilin <52244434+zhilinn@users.noreply.github.com>
2019-06-26 21:25:24 +0300

    Reorder options.

--- Source/options.c
+++ Source/options.c
@@ -4,22 +4,22 @@
 #include "options.h"
 
 void parse_options(OPTIONS* options, int argc, char** argv) {
-	options->verbose = false;
-	options->recursive = false;
-	options->print_positions = false;
 	options->ignore_case = false;
+	options->print_positions = false;
+	options->recursive = false;
 	options->show_stats = false;
+	options->verbose = false;
 
 	int argn = 0;
 	for (int i = 1; i < argc; i++) {
 		if (argv[i][0] == '-') {
 			for (int j = 1; argv[i][j]; j++) {
 				switch (argv[i][j]) {
-				case 'v': options->verbose = true; break;
-				case 'r': options->recursive = true; break;
-				case 'p': options->print_positions = true; break;
 				case 'i': options->ignore_case = true; break;
+				case 'p': options->print_positions = true; break;
+				case 'r': options->recursive = true; break;
 				case 's': options->show_stats = true; break;
+				case 'v': options->verbose = true; break;
 				}
 			}
 		}
@@ -34,11 +34,11 @@ void parse_options(OPTIONS* options, int argc, char** argv) {
 	if (argn != 2) {
 		fprintf(stderr,
 			"Usage: %s [options] <directory> <text>\n"
-			"  -v: Verbose\n"
-			"  -r: Recursive\n"
-			"  -p: Print positions\n"
 			"  -i: Ignore case\n"
+			"  -p: Print positions\n"
+			"  -r: Recursive\n"
 			"  -s: Show statistics\n"
+			"  -v: Verbose\n"
 			"\n",
 			argv[0]);
 
